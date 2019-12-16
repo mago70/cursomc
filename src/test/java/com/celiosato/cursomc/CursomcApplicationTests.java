0b6b73afb7c6f7cd66655f061ae93c43a91bb86c
Celio Sato <mago70@gmail.com>
2019-12-16 16:41:38 -0300

    Projeto criado

---
+++ src/test/java/com/celiosato/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celiosato.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
