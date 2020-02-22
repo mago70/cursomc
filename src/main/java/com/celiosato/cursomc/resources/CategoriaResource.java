d4fa7e6f376cfcf28fbeba8135fe984e10601333
Celio Sato <mago70@gmail.com>
2020-02-22 17:18:59 -0300

    Atualizando uma Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/celiosato/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/celiosato/cursomc/resources/CategoriaResource.java
@@ -26,18 +26,25 @@ public class CategoriaResource {
 	private CategoriaService service;
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)// verbos GET, POST, DELETE
-	public ResponseEntity<?> find(@PathVariable Integer id){
-		Categoria obj = service.buscar(id);
+	public ResponseEntity<Categoria> find(@PathVariable Integer id){
+		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
 	
-	@RequestMapping(method = RequestMethod.POST)
+	@RequestMapping(method=RequestMethod.POST)
 	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 		
+	}
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
 	}
 	
 }
--- src/main/java/com/celiosato/cursomc/resources/PedidoResource.java
+++ src/main/java/com/celiosato/cursomc/resources/PedidoResource.java
@@ -22,8 +22,8 @@ public class PedidoResource {
 	private PedidoService service;
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)// verbos GET, POST, DELETE
-	public ResponseEntity<?> find(@PathVariable Integer id){
-		Pedido obj = service.buscar(id);
+	public ResponseEntity<Pedido> find(@PathVariable Integer id){
+		Pedido obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
 	
