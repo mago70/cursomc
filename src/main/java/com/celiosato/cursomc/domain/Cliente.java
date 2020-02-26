198debfcc7d4685e9c3d45db170b262fbf759517
Celio Sato <mago70@gmail.com>
2020-02-26 14:31:22 -0300

    PUT, DELETE, GET para Cliente

--- src/main/java/com/celiosato/cursomc/domain/Cliente.java
+++ src/main/java/com/celiosato/cursomc/domain/Cliente.java
@@ -50,7 +50,7 @@ public class Cliente implements Serializable{
 		this.nome = nome;
 		Email = email;
 		this.cpfOuCnpj = cpfouCnpj;
-		this.tipo = tipo.getCod();
+		this.tipo = (tipo == null) ? null : tipo.getCod();
 	}
 
 	public Integer getId() {
