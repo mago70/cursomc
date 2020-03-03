a43b92a9698a5ba26c09a330db79f783940d09ea
Celio Sato <mago70@gmail.com>
2020-03-03 16:47:03 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/celiosato/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/celiosato/cursomc/repositories/ProdutoRepository.java
@@ -1,8 +1,17 @@
 package com.celiosato.cursomc.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.celiosato.cursomc.domain.Categoria;
 import com.celiosato.cursomc.domain.Produto;
 
 // Objeto da camada de acesso a dados referente ao tipo categoria
@@ -10,5 +19,10 @@ import com.celiosato.cursomc.domain.Produto;
 @Repository		         								//Objeto   , tipo do atributo identificador
 public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
 	
-
+	@Transactional(readOnly = true)
+	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	Page<Produto> search(@Param("nome")String nome, @Param("categorias")List<Categoria>categorias, Pageable pageRequest);
+	
+	//trocar o nome no ProdutoService para findDistinctByNomeContainingAndCategorias Invés do "search"
+	//Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria>categorias, Pageable pageRequest);
 }
---
+++ src/main/java/com/celiosato/cursomc/services/ProdutoService.java
@@ -0,0 +1,40 @@
+package com.celiosato.cursomc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.celiosato.cursomc.domain.Categoria;
+import com.celiosato.cursomc.domain.Produto;
+import com.celiosato.cursomc.repositories.CategoriaRepository;
+import com.celiosato.cursomc.repositories.ProdutoRepository;
+import com.celiosato.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+	
+	@Autowired 
+	private ProdutoRepository repo;
+	
+	@Autowired 
+	private CategoriaRepository categoriaRepository;
+	
+	public Produto find(Integer id) {
+		Optional<Produto> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
+	}
+	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return repo.search(nome, categorias, pageRequest);
+		
+	}
+
+}
