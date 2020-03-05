45d1c57fcf853046bc64f14f50b19800555f27b9
Celio Sato <mago70@gmail.com>
2020-03-05 14:34:11 -0300

    Criando profile de desenvolvimento

--- src/main/java/com/celiosato/cursomc/config/DevConfig.java
+++ src/main/java/com/celiosato/cursomc/config/DevConfig.java
@@ -3,7 +3,6 @@ package com.celiosato.cursomc.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -11,22 +10,14 @@ import org.springframework.context.annotation.Profile;
 import com.celiosato.cursomc.services.DBService;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 	
 	@Autowired
 	private DBService dbService;
-	
-	@Value("${spring.jpa.hibernate.ddl-auto}")
-	private String strategy;
 
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
-		
-		if(!"create".equals(strategy) ) {
-			return false;
-		}
-		
 		dbService.instantiateTestDatabase();
 		return true;
 	}
