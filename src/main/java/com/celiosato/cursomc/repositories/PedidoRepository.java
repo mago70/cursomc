bb1cf5fb85cbdfb883d98b7f95e0e326bb7deed1
Celio Sato <mago70@gmail.com>
2020-03-14 16:50:14 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/celiosato/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/celiosato/cursomc/repositories/PedidoRepository.java
@@ -1,8 +1,12 @@
 package com.celiosato.cursomc.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.celiosato.cursomc.domain.Cliente;
 import com.celiosato.cursomc.domain.Pedido;
 
 // Objeto da camada de acesso a dados referente ao tipo categoria
@@ -10,5 +14,7 @@ import com.celiosato.cursomc.domain.Pedido;
 @Repository		         								//Objeto   , tipo do atributo identificador
 public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
 	
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 
 }
