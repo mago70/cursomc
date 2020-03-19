313b3ac70f5124fad6d70d6f01b8401572ebf31b
Celio Sato <mago70@gmail.com>
2020-03-19 12:17:18 -0300

    Enviando imagem via endpoint

--- src/main/java/com/celiosato/cursomc/services/ClienteService.java
+++ src/main/java/com/celiosato/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.celiosato.cursomc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.celiosato.cursomc.domain.Cidade;
 import com.celiosato.cursomc.domain.Cliente;
@@ -37,6 +39,9 @@ public class ClienteService {
 	
 	@Autowired
 	private BCryptPasswordEncoder pe;
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	
 	public Cliente find(Integer id) {
@@ -109,4 +114,7 @@ public class ClienteService {
 		newObj.setEmail(obj.getEmail());
 	}
 
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
+	}
 }
