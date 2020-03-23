be564edbcdf09d619f9545d11d37d812cf6c4a58
Celio Sato <mago70@gmail.com>
2020-03-23 14:30:40 -0300

    Aumentando o tamanho maximo permitido para upload

--- src/main/java/com/celiosato/cursomc/config/SecurityConfig.java
+++ src/main/java/com/celiosato/cursomc/config/SecurityConfig.java
@@ -52,14 +52,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			"/auth/forgot/**"
 	};
 	
-	
-	private static final String[] PUBLIC_MATCHERS_PUT = {
-			"/categorias/**"
-			
-	};
-	
-	
-	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
 		
@@ -71,7 +63,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		http.authorizeRequests()
 			.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
-			.antMatchers(HttpMethod.PUT, PUBLIC_MATCHERS_PUT).permitAll()
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
